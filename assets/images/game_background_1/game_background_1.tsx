744fb6c268e6eb10f23bef34ff6a6be772dd8c44
Jam19th <jam19th@gmail.com>
2023-05-31 15:17:18 -0700

    Added tsx files and collision array

---
+++ assets/images/game_background_1/game_background_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="game_background_1" tilewidth="36" tileheight="27" tilecount="2120" columns="53">
+ <image source="game_background_1.png" width="1920" height="1080"/>
+</tileset>
--- assets/index.js
+++ assets/index.js
@@ -3,11 +3,46 @@ const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
 //determines canvas size 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1270
+canvas.height = 610
+
+
+const platformCollisions2D = []
+for (let i=0; i< platformCollisions.length; i += 36) {
+    platformCollisions2D.push(platformCollisions.slice(i, i + 36))
+}
+
+console.log(platformCollisions2D)
 
 //variable to determine strength of gravity
 const gravity = .5
+
+// Loads the image and draws the sprite onto canvas
+class Sprite {
+    constructor({position, imageSrc}) {
+        this.position = position
+        this.image = new Image()
+        this.image.src = imageSrc
+    }
+    // method to draw sprites onto canvas
+    draw () {
+        if (!this.image) return
+        context.drawImage(this.image, this.position.x, this.position.y)
+    }
+    // This will call the draw method and redraw the sprite on the canvas.
+    update () {
+        this.draw()
+    }
+}
+
+// loads background sprite
+const background = new Sprite({
+    position: {
+        x: 0, 
+        y: 0
+    },
+    imageSrc: './assets/images/game_background_1/game_background_1.png'
+});
 
 // Fills shapes with a color
 context.fillStyle = '#FFFFFF';
@@ -15,6 +50,7 @@ context.fillStyle = '#FFFFFF';
 //creates a rectangle and determines the size of it(starting x position, starting y position , width, height)
 context.fillRect(5, 5, canvas.width, canvas.height)
 
+// Creates a variable and assigns properties to the movement codes
 const codes = {
     arrowRight: {
         pressed: false,
@@ -37,6 +73,13 @@ function animatePlayer() {
 
     //creates a rectangle and determines the size of it(starting x position, starting y position , width, height)
     context.fillRect(0, 0, canvas.width, canvas.height)
+
+    // save and restore prevents the code from refreshing forever 
+    context.save();
+    context.translate(0, -background.image.height + canvas.height);
+    //Puts the background images onto canvas
+    background.update()
+    context.restore();
 
     //Calls the draw and update function to o draw the player character and update the position of the player character
     player.update()
